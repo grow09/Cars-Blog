d266b658c1cf7ca9435acf6eb6655313cc9e65ae
grow09 <grovexd94@gmail.com>
2021-08-27 05:06:07 +0300

    commit

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,14 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
-    path('cats/<int:catid>/', categories),
-    path('archive/<int:year>/', categories),
-    path('main/', main),
+    path('', index, name='home'),
+    path('about/', about, name='about'),
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post'),
+    path('category/<int:cat_id>/', show_category, name='category'),
+    # path('cats/<int:catid>/', categories),
+    # path('archive/<int:year>/', categories),
+    # path('main/', main),
 ]
--- main/views.py
+++ main/views.py
@@ -7,21 +7,67 @@ from .models import *
 
 # Create your views here.
 
-main_menu = ['Main page', 'About', 'Contacts']
+main_menu = [{'title': 'About', 'url_name': 'about'},
+            {'title': 'Add article', 'url_name': 'add_page'},
+            {'title': 'Contacts', 'url_name': 'contact'},
+            {'title': 'Login', 'url_name': 'login'}
+]
+
 
 def index(request):
-    return render(request, 'main/index.html', {'main_menu': main_menu, 'title': 'Main page'})
-
-
-def main(request):
     posts = Cars.objects.all()
-    return render(request, 'main/main.html', {'posts': posts, 'main_menu': main_menu, 'title': 'Main'})
+    cats = Category.objects.all()
+    context = {'posts': posts,
+               'cats': cats,
+               'main_menu': main_menu,
+               'title': 'Main',
+               'cat_selected': 0
+   }
+
+    return render(request, 'main/main.html', context=context)
 
 
-def categories(request, year):
-    if int(year) > datetime.now().year:
-        return redirect('/archive/'+ datetime.now().year +'/', permanent=True)
-    return HttpResponse(f"Выбран {year} год.")
+def show_category(request, cat_id):
+    posts = Cars.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if cat_id > len(cats) or cat_id == 0:
+        raise Http404()
+
+    context = {'posts': posts,
+               'cats': cats,
+               'main_menu': main_menu,
+               'title': cats[cat_id-1],
+               'cat_selected': cat_id,
+               }
+
+    return render(request, 'main/main.html', context=context)
+
+
+def show_post(request, post_id):
+    return HttpResponse(f'Post with id = {post_id}')
+
+
+def about(request):
+    return render(request, 'main/about.html', {'main_menu': main_menu, 'title': 'About'})
+
+
+def addpage(request):
+    return HttpResponse('Add article')
+
+
+def contact(request):
+    return HttpResponse('Contacts')
+
+
+def login(request):
+    return HttpResponse('Login')
+
+
+# def categories(request, year):
+#     if int(year) > datetime.now().year:
+#         return redirect('/archive/'+ datetime.now().year +'/', permanent=True)
+#     return HttpResponse(f"Выбран {year} год.")
 
 
 def pageNotFound(request, exception):
