4035239e4bbbd03e64e4de61b6fa47f59e639391
grow09 <grovexd94@gmail.com>
2021-09-01 19:17:06 +0300

    commit

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from datetime import datetime
 from django.shortcuts import redirect
 from .models import *
@@ -43,7 +43,16 @@ def show_category(request, cat_id):
 
 
 def show_post(request, post_id):
-    return HttpResponse(f'Post with id = {post_id}')
+    post = get_object_or_404(Car, pk=post_id)
+
+    context = {
+        'post': post,
+        'main_menu': main_menu,
+        'title': post.title,
+        'cat_selected': post.cat_id,
+    }
+
+    return render(request, 'main/post.html', context=context)
 
 
 def about(request):
